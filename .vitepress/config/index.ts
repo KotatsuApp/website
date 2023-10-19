98464be1a3c8c490c4c3ae72d77e48b9dfd6b32a
Zakhar Timoshenko <vp1984tanki@gmail.com>
2023-10-20 00:51:02 +0300

    Fix favicon

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -62,7 +62,7 @@ export default defineConfigWithTheme<Theme.Config>({
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' }],
 
     ['link', { rel: 'icon', href: '/favicon.ico?v=2', sizes: 'any' }],
-    ['link', { rel: 'icon', href: '/logo.svg?v=2', type: 'image/svg+xml' }],
+    ['link', { rel: 'icon', href: '/logo-compact.svg?v=2', type: 'image/svg+xml' }],
     ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png?v=2' }],
     ['link', { rel: 'manifest', href: '/site.webmanifest' }],
   ],
