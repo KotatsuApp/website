33dfd52adda9b3df936120b871f5669000052cac
ztimms73 <xtimms@live.com>
2024-08-01 22:59:05 +0000

    Deploying to gh-pages from @ KotatsuApp/website@46602c3ed8f3de222bde6cf05f83983633dea3c6 🚀

--- assets/chunks/add-to-favourites.5becd14b.js
+++
@@ -1,1 +0,0 @@
-const t="/manuals/guides/getting-started/welcome.png",s="/manuals/guides/getting-started/add-to-favourites.png";export{t as _,s as a};
---
+++ assets/chunks/add-to-favourites.a42367ee.js
@@ -0,0 +1,1 @@
+const t="/manuals/guides/getting-started/welcome.png",s="/manuals/guides/getting-started/add-to-favourites.jpg";export{t as _,s as a};
